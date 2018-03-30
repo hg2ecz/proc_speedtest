c8e1ea6180a8ce5cf58b29e8734a7bef1d731bbf
HaaZee <haazee4droid@gmail.com>
2018-03-30 22:11:40 +0200

    Java kód módosítva, egyszerűsítve

--- Proc_Speed_Test.java
+++ Proc_Speed_Test.java
@@ -6,28 +6,9 @@ import java.nio.file.*;
 
 public class Proc_Speed_Test {
 
-	public void run_test_0(){
-		int ctr = 0, err = 0;
-		File file = null;
-		String[] paths;
-		
-		try {
-			file=new File("/proc/");
-			System.out.println("File object created");
-			paths = file.list();
-			for(String p:paths){
-				System.out.println(p);
-			}
-		} catch(Exception x){
-			System.out.println(x);
-		}
-		
-		System.out.println("Hello, it's running");
-	}
 	
 	public void run_test(){
 			int ctr=0, err=0;
-			String line;
 
 			for(int i=0; i<10000; i++){
 				Path dir = Paths.get("/proc");
@@ -35,16 +16,11 @@ public class Proc_Speed_Test {
 						Files.newDirectoryStream(dir,"[0-9]*")){
 							for(Path path : stream){
 								String pathname = path.toString();
-									try {
-										BufferedReader br;
-										br=new BufferedReader(
-											new FileReader(pathname+"/stat")
-										);
-										while((line=br.readLine()) != null){
-											// ----
-										}
-										br.close();
-										ctr++;
+									try ( BufferedReader br = new BufferedReader( new FileReader(pathname+"/stat")) )
+									{
+											String line;
+											line = br.readLine();
+											ctr++;
 									} catch(IOException e){
 										err++;
 									}
