4a56164e655d4e36a6310e92e25033492b56d928
HaaZee <haazee4droid@gmail.com>
2018-03-31 00:39:41 +0200

    Java optimalizálás, a korábbi méréseket kukaéretté tette

--- Proc_Speed_Test.java
+++ Proc_Speed_Test.java
@@ -4,34 +4,40 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.*;
 
+import java.lang.reflect.Method;
+
 public class Proc_Speed_Test {
 
 	
 	public void run_test(){
 			int ctr=0, err=0;
+			Path dir;
+			try {
+				dir = Paths.get("/proc");
 
-			for(int i=0; i<10000; i++){
-				Path dir = Paths.get("/proc");
-				try (DirectoryStream<Path> stream =
-						Files.newDirectoryStream(dir,"[0-9]*")){
+					for(int i=0; i<10000; i++){
+						try {
+							DirectoryStream<Path> stream = Files.newDirectoryStream( dir, "[0-9]*" );
+
 							for(Path path : stream){
-								String pathname = path.toString();
-									try ( BufferedReader br = new BufferedReader( new FileReader(pathname+"/stat")) )
-									{
-											String line;
-											line = br.readLine();
-											ctr++;
-									} catch(IOException e){
-										err++;
-									}
+								Path path2file = path.resolve("stat");
+								try ( BufferedReader br = Files.newBufferedReader( path ) )
+								{
+										String line;
+										line = br.readLine();
+										ctr++;
+								} catch(IOException e){
+									err++;
+								}
 							}
-				} catch(Exception e) {
-					System.out.println("--Error--");
-					System.out.println(e);
-				}
-			}
-			System.out.format("Count: %d%n",ctr);
-			System.out.format("Error: %d%n",err);
+						} catch(Exception e) {
+							System.out.println("--Error--");
+							System.out.println(e);
+						}
+					}
+					System.out.format("Count: %d%n",ctr);
+					System.out.format("Error: %d%n",err);
+			} catch(Exception e) { }
 	}
 
 
